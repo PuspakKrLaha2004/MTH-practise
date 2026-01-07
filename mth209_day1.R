4ff6d3a47960ebf3f7587496ab7eb9141c539017
PuspakKrLaha2004 <puspakkmr25@iitk.ac.in>
2026-01-07 15:00:39 +0530

    Update mth209_day1.R

--- mth209_day1.R
+++ mth209_day1.R
@@ -12,3 +12,52 @@ for (k in 1:3)
 }
 sum
 all.equal(sum, truth)
+
+# Q2
+n <- 1000
+A <- matrix(runif(n^2), nrow = n, ncol = n)
+time <- system.time(A%*%A)[3]
+time
+
+# Q3
+times <- numeric(length = 7)
+v <- c(1e2, 5e2, 1e3, 2e3, 3e3, 4e3, 5e3) 
+for (i in 1:7){
+  n <- v[i]
+  A <- matrix(runif(v[i]^2), nrow = n, ncol = n)
+  times[i] <- system.time(A%*%A)[3]
+}
+
+plot(v,times)
+
+# Q4
+n <- 1000
+A <- matrix(runif(n^2), nrow=n, ncol=n)
+A <- t(A) %*% A
+
+## standard
+solve.A.inv <- solve(A)
+solve.A.inv %*% A
+time <- system.time(solve.A.inv)
+time
+
+## qr solve
+A.inv <- qr.solve(A)
+time <- system.time(A.inv)
+time
+
+## cholesky
+# Cholesky decomposition
+R <- chol(A) # A = R^T R
+# Inverse via Cholesky
+A.inv <- chol2inv(R)
+
+## Singular Value Decomposition
+svd_A <- svd(A)
+
+U <- svd_A$u
+D <- svd_A$d
+V <- svd_A$v
+
+# Inverse via SVD
+A.inv <- V %*% diag(1 / D) %*% t(U)
